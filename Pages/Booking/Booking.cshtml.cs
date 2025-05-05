91b7b5cf33724f9afa5c1f964a7756579f059eb5
Wess L. Toft <weslyn.l.toft@gmail.com>
2025-05-05 16:48:02 +0200

    Ready for Database connection and Colors on page fixed c:

---
+++ Pages/Booking/Booking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Case_2___Zealand_Lokale_Booking.Pages.Booking
+{
+    public class BookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
